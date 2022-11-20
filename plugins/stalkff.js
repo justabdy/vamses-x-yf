2719fed613e6e960dbe0e540d81be075547c855a
Vamses <108464869+VamsesOfficial@users.noreply.github.com>
2022-11-20 08:27:25 +0800

    Add files via upload

---
+++ plugins/stalkff.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+async function stalkff(text) {
+  var datap = {
+    "voucherPricePoint.id": 8050,
+    "voucherPricePoint.price": "",
+    "voucherPricePoint.variablePrice": "",
+    "email": "",
+    "n": "",
+    "userVariablePrice": "",
+    "order.data.profile": "",
+    "user.userId": text,
+    "voucherTypeName": "FREEFIRE",
+    "affiliateTrackingId": "",
+    "impactClickId": "",
+    "checkoutId": "",
+    "tmwAccessToken": "",
+    "shopLang": "in_ID",
+  }
+  var epep = await axios({
+    "headers": {
+    "Content-Type": "application/json; charset\u003dutf-8"
+    },
+    "method": "POST",
+    "url": "https://order.codashop.com/id/initPayment.action",
+    "data": datap
+  })
+  return {
+    id: text,
+    nickname: epep.data["confirmationFields"]["roles"][0]["role"]
+  }
+}
+module.exports = { stalkff }
